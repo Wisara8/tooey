b05e4fbf574396f8d58ff80b5bd674af53dfdc1e
wisara88 <srandall845@gmail.com>
2021-02-25 14:02:31 -0800

    column update

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,11 +3,46 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.lightBlueAccent,
-        body: Column(
-          children: <Widget>[Icon(Icons.list), Text("Todooey Tooey")],
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      body: Container(
+        padding: EdgeInsets.only(top: 60, bottom: 30, left: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30,
+              ),
+              radius: 30,
+              backgroundColor: Colors.white,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Todooey Tooey",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w700),
+            ),
+            Text(
+              "12 Tasks",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
