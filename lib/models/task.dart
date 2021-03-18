c96195d6742f75fbe90532ed9a0e5e11c5f5f8d7
wisara88 <srandall845@gmail.com>
2021-03-18 10:32:16 -0700

    reseting tasks

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,8 +1,8 @@
 class Task {
   final String name;
   bool isDone;
-  final int count;
-  Task({this.name, this.isDone = false, this.count});
+
+  Task({this.name, this.isDone = false});
 
   void toggleDone() {
     isDone = !isDone;
