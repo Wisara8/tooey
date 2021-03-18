d0a5a02c8319d88ef685ced7751cf39e53269d6e
wisara88 <srandall845@gmail.com>
2021-03-17 23:46:48 -0700

    .

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,8 +1,8 @@
 class Task {
   final String name;
   bool isDone;
-
-  Task({this.name, this.isDone = false});
+  final int count;
+  Task({this.name, this.isDone = false, this.count});
 
   void toggleDone() {
     isDone = !isDone;
