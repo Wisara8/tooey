9b542868e3441f629f26e04fcb85d9f9c8c4c85e
wisara88 <srandall845@gmail.com>
2021-03-22 23:23:05 -0700

    .2

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -61,7 +61,7 @@ class _TasksScreenState extends State<TasksScreen> {
                   height: 10,
                 ),
                 Text(
-                  "Todooey Tooey V2",
+                  "Todooey Tooey V3",
                   style: TextStyle(
                       color: Colors.white,
                       fontSize: 30,
