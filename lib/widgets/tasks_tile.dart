d100aa9cb265816746c2dbff9b5f94dd31cb2e1b
wisara88 <srandall845@gmail.com>
2021-03-26 16:12:02 -0700

    deleting with provider

--- lib/widgets/tasks_tile.dart
+++ lib/widgets/tasks_tile.dart
@@ -4,12 +4,18 @@ class TaskTile extends StatelessWidget {
   final bool isChecked;
   final String taskTitle;
   final Function checkboxCallback;
+  final Function onLongPressCallback;
 
-  TaskTile({this.isChecked, this.taskTitle, this.checkboxCallback});
+  TaskTile(
+      {this.isChecked,
+      this.taskTitle,
+      this.checkboxCallback,
+      this.onLongPressCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onLongPress: onLongPressCallback,
       title: Text(
         taskTitle,
         style: TextStyle(
